c42117446e9bcd92283e323fc057bfa44bd6848c
James Qiu <qiujames@cs.washington.edu>
2021-10-23 15:16:06 -0700

    my commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_from_directory
+from flask_restful import Api
+from flask_cors import CORS #comment this on deployment
+
+from api.SalaryAPI import HelloApiHandler
+
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+CORS(app) #comment this on deployment
+api = Api(app)
+
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+api.add_resource(HelloApiHandler, '/flask/salary')
+
+
+if __name__ == '__main__':
+    app.run()
