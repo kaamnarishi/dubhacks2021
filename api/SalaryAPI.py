56374bc30042ff19fc3defb83b2976f607ffbce2
James Qiu <qiujames@cs.washington.edu>
2021-10-23 18:02:51 -0700

    Add avg state rent costs + monthly base adjusted gross income

--- api/SalaryAPI.py
+++ api/SalaryAPI.py
@@ -1,3 +1,4 @@
+import csv
 import statistics
 import time
 
@@ -45,6 +46,7 @@ class SalaryApiHandler(Resource):
     driver.find_element(By.XPATH, f"//div/label[contains(text(), '{TIME_FRAME}')]").click()
 
     total_salaries = []
+    total_base_salaries = []
     time.sleep(0.5)
     # Go through the table and append to total salaries
     trs = driver.find_elements(By.XPATH, "//table[@id='compTable']/tbody/tr")
@@ -53,9 +55,11 @@ class SalaryApiHandler(Resource):
         location = t[1]
         experience = t[4]
         salary = t[5]
+        base_salary = t[6].split('|')[0]
         if experience == '0 / 0' and location.split('|')[0].split(',')[1].strip() == STATE:
             print(location, experience)
             total_salaries.append(int(salary[1:].replace(',', '')))
+            total_base_salaries.append(int(base_salary.replace(',', '').replace('k', '000')))
 
     try:
         number_of_pages = int(driver.find_element(By.XPATH, "//ul[@class='pagination']/li[last()-1]").text)
@@ -72,25 +76,40 @@ class SalaryApiHandler(Resource):
             experience = t[4]
             salary = t[5]
             if experience == '0 / 0' and location.split('|')[0].split(',')[1].strip() == STATE:
-                print(location, experience)
                 total_salaries.append(int(salary[1:].replace(',', '')))
 
-    print(total_salaries)
     print(statistics.mean(total_salaries))
-    avgSalary = round(statistics.mean(total_salaries))
+    print(statistics.mean(total_base_salaries))
+    avgAnnualSalary = round(statistics.mean(total_salaries))
+    avgBaseSalary = round(statistics.mean(total_base_salaries))
 
-    my_headers = {'Authorization': 'Bearer apiKey'}
+    # GET TAX STATISTICS
+    my_headers = {'Authorization': 'Bearer apiToken'}
     response = requests.post(
         'https://taxee.io/api/v2/calculate/2020',
         headers=my_headers,
-        data={"pay_rate": avgSalary, "filing_status": "single", "state": "WA"},
+        data={"pay_rate": avgAnnualSalary, "filing_status": "single", "state": "WA"},
     )
 
     annual_taxes = response.json()["annual"]
     annual_state_tax = annual_taxes["state"]["amount"] if annual_taxes["state"]["amount"] else 0
     annual_federal_tax = annual_taxes["federal"]["amount"]
     annual_fica_tax = annual_taxes["fica"]["amount"]
-    annualAGI = avgSalary - annual_state_tax - annual_federal_tax - annual_fica_tax
+    annualAGI = avgAnnualSalary - annual_state_tax - annual_federal_tax - annual_fica_tax
+
+    taxRate = annualAGI / avgAnnualSalary
+    print("TAX", taxRate)
+    annualBaseAGI = taxRate * avgBaseSalary
+    print("Annual contributed by base salary", annualBaseAGI)
+    monthlyBaseAGI = annualBaseAGI / 12
+
+    # GET AVERAGE RENT PRICES
+    state_to_rent = dict()
+    with open("api/stateRent.csv") as stateRentFile:
+        tsv_file = csv.reader(stateRentFile, delimiter="\t")
+        for line in tsv_file:
+            state_to_rent[line[0]] = (line[1], line[2], line[3])
+
 
     return {
         'resultStatus': 'SUCCESS',
@@ -103,5 +122,9 @@ class SalaryApiHandler(Resource):
         'annualFederalTax': annual_federal_tax,
         'annualFicaTax': annual_fica_tax,
         'annualAGI': annualAGI,
-        'monthlyAGI': annualAGI / 12
+        'monthlyAGI': annualAGI / 12,
+        'monthlyBaseAGI': monthlyBaseAGI,
+        'avgStudioRent': state_to_rent[STATE][0],
+        'avgOneBdrmRent': state_to_rent[STATE][1],
+        'avgTwoBdrmRent': state_to_rent[STATE][2]
     }
