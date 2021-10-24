cda8546441580acf9ae9854289be956a95613277
Megha Jain <mj2905@uw.edu>
2021-10-24 03:45:02 -0700

    the pie charts edit

--- frontend/src/PieCharts.js
+++ frontend/src/PieCharts.js
@@ -4,18 +4,18 @@ import { PieChart, Pie, Cell} from 'recharts';
 
 export function PieCharts(props) {
   // Sample data
-  const data = [
-    {type: 'Total Spending', amount: 400},
-    {type: 'Total Saving', amount: 600},
-    {type: 'Leftover', amount: 290}
+  const GoogleWA = [
+    {type: 'Total Spending', amount: .45},
+    {type: 'Total Saving', amount: .40},
+    {type: 'Leftover', amount: .15}
   ];
 
   const COLORS = ['#635DDF', '#D26236', '#83DDD4'];
 
   return (
     <PieChart width={600} height={600}>
-      <Pie data={data} dataKey="amount" outerRadius={200} fill="#8F8AF8, green, red">
-        {data.map((entry, index) => (
+      <Pie data={GoogleWA} dataKey="amount" outerRadius={200} fill="#8F8AF8, green, red">
+        {GoogleWA.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
         ))}
       </Pie>
